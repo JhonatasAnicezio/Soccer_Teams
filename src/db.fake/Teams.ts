d47989cb22fc067c4e3c7ce9dbbe2bbc59a35c37
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-04-21 10:11:19 -0300

    feat(db.fake): create array for mocks and bd

---
+++ src/db.fake/Teams.ts
@@ -0,0 +1,19 @@
+const teamsSoccer = [
+  {
+    name: "Alemanha",
+    abbreviation: "ger",
+    leagues: 3,
+  },
+  {
+    name: "Brasil",
+    abbreviation: "bra",
+    leagues: 3,
+  },
+  {
+    name: "França",
+    abbreviation: "fra",
+    leagues: 3,
+  },
+];
+
+export default teamsSoccer;
