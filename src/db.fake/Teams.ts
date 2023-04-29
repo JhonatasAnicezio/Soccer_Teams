5a47386bb984f73bb0fd6966c1bd00838d4eed8f
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-04-29 13:20:31 -0300

    feat(db.fake): add teams for compilation

--- src/db.fake/Teams.ts
+++ src/db.fake/Teams.ts
@@ -1,8 +1,23 @@
 const teamsSoccer = [
   {
-    name: 'Alemanha',
-    abbreviation: 'ger',
-    leagues: 2,
+    name: 'Argentina',
+    abbreviation: 'arg',
+    leagues: 5,
+  },
+  {
+    name: 'Austrália',
+    abbreviation: 'aus',
+    leagues: 1,
+  },
+  {
+    name: 'Áustria',
+    abbreviation: 'aut',
+    leagues: 1,
+  },
+  {
+    name: 'Bélgica',
+    abbreviation: 'bel',
+    leagues: 1,
   },
   {
     name: 'Brasil',
@@ -10,9 +25,89 @@ const teamsSoccer = [
     leagues: 3,
   },
   {
+    name: 'China',
+    abbreviation: 'chn',
+    leagues: 1,
+  },
+  {
+    name: 'Dinamarca',
+    abbreviation: 'den',
+    leagues: 1,
+  },
+  {
+    name: 'Inglaterra',
+    abbreviation: 'eng',
+    leagues: 5,
+  },
+  {
     name: 'França',
     abbreviation: 'fra',
     leagues: 2,
+  },
+  {
+    name: 'Alemanha',
+    abbreviation: 'ger',
+    leagues: 2,
+  },
+  {
+    name: 'Gana',
+    abbreviation: 'gha',
+    leagues: 1,
+  },
+  {
+    name: 'Irlanda',
+    abbreviation: 'irl',
+    leagues: 1,
+  },
+  {
+    name: 'Itália',
+    abbreviation: 'ita',
+    leagues: 2,
+  },
+  {
+    name: 'México',
+    abbreviation: 'mex',
+    leagues: 2,
+  },
+  {
+    name: 'Holanda',
+    abbreviation: 'ned',
+    leagues: 3,
+  },
+  {
+    name: 'Irlanda do Norte',
+    abbreviation: 'nir',
+    leagues: 1,
+  },
+  {
+    name: 'Noruega',
+    abbreviation: 'nor',
+    leagues: 1,
+  },
+  {
+    name: 'Romênia',
+    abbreviation: 'rou',
+    leagues: 1,
+  },
+  {
+    name: 'Escócia',
+    abbreviation: 'sco',
+    leagues: 4,
+  },
+  {
+    name: 'Espanha',
+    abbreviation: 'esp',
+    leagues: 2,
+  },
+  {
+    name: 'Suécia',
+    abbreviation: 'swe',
+    leagues: 1,
+  },
+  {
+    name: 'País de Gales',
+    abbreviation: 'wal',
+    leagues: 1,
   },
 ];
 
